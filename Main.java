d6616ee97d69c15f116fb2605494357b180275c1
Arthur Furukawa <89026599+Arthur-Furukawa@users.noreply.github.com>
2024-09-21 16:24:02 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,77 @@
+package org.example;
+
+/*
+Referências
+https://www.geeksforgeeks.org/binary-tree-data-structure/
+https://www.geeksforgeeks.org/convert-infix-expression-to-postfix-expression/
+https://www.geeksforgeeks.org/program-to-convert-infix-notation-to-expression-tree/
+
+
+Integrantes:
+Arthur Ryuiti Sato Furukawa RA: 10409106
+Felipe Jiao                 RA:
+*/
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int section = 0;
+            String infixExpression = "";
+            ExpressionTree tree;
+            tree = new ExpressionTree();
+            Scanner scanner = new Scanner(System.in);
+
+            boolean used2option = false;
+
+
+            while (section != 5) {
+                System.out.println("Qual seção deseja acessar?: ");
+                section = scanner.nextInt();
+                scanner.nextLine();
+
+                if (section == 1) {
+                    try {
+                        System.out.println("Qual o valor da expressao infixa ?: ");
+                        String infixExpressionInput = scanner.nextLine().replaceAll("[\\s\\t]+", "");;
+                        infixExpression = infixExpressionInput;
+
+                    }catch (Exception Exception){
+                        System.out.println("Epa, parece que a expressao inserida é invalida, tente novamente");
+                    }
+                }
+//                String expression = "0.5*3/0.25";
+                if (section == 2) {
+                    try {
+                        tree.createExpressionTree(infixExpression);
+                        System.out.println("Arvore de expressão gerada com sucesso");
+                        used2option = true;
+                    }catch (Exception Exception){
+                        System.out.println("Epa, insira uma expressao valida antes utilizando a opcao 1");
+                    }
+                }
+                if (section == 3) {
+                    if(used2option == true) {
+                        tree.printInOrder();
+                        tree.printPostOrder();
+                        tree.printPreOrder();
+                    }else{
+                        System.out.println("Epa,utilize primeiro a opção 2");
+                    }
+                }
+                if (section == 4) {
+                    if(used2option == true) {
+                        tree.evaluate();
+                    }else{
+                        System.out.println("Epa,utilize primeiro a opção 2");
+                    }
+                }
+            }
+        }catch (Exception exception){
+            System.out.println("Epa algo deu errado ;-; tente novamente!");
+        }
+        System.out.println("Obrigado por utilizar nosso Software UwU");
+    }
+}
---
+++ Node.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Node {
+
+    private String value;
+//    private Node parentNode;
+
+    private Node leftSon;
+
+    private Node rightSon;
+
+    public Node (String  data){
+        this.value = data;
+
+        this.leftSon =null;
+        this.rightSon = null;
+    }
+
+    public void setLeftSon(Node leftSon) {
+        this.leftSon = leftSon;
+    }
+
+    public void setRightSon(Node rightSon) {
+        this.rightSon = rightSon;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public float visitar(){return Float.parseFloat(value);}
+
+    public Node getLeftSon() {
+        return leftSon;
+    }
+
+    public Node getRightSon() {
+        return rightSon;
+    }
+}
